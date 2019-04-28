4cd900d32e753fe52c52dcedb5913bd428f97251
James Arnold <james@pairgo.co.uk>
2019-04-28 18:14:50 +0100

    Added modifier gameplay and pause menu

---
+++ GameJamLudumDare/Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject ui;
+    public GameObject toolTip;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Toggle();
+        }        
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            toolTip.SetActive(false);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            toolTip.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+
+    }
+}
