55668e7af6829f3621defd03bbc55d954fc25165
David Ward <scifa13@hotmail.com>
2019-04-29 21:45:18 +0100

    Game over added
    Co-Authored-By: Antoine Colombier <acolombier@users.noreply.github.com>

---
+++ GameJamLudumDare/Assets/Script/GameOverEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverEvent : MonoBehaviour
+{
+
+    public GameObject gameOverScreen;
+    public GameObject RestartButton;
+
+    public void Start()
+    {
+        EventManager.StartListening("game_over", Toggle);
+        gameOverScreen.SetActive(false);
+        RestartButton.SetActive(false);
+    }
+
+    public void Toggle()
+    {
+        Toggle(new object[] { });
+    }
+
+    public void Toggle(object[] args)
+    {
+        gameOverScreen.SetActive(true);
+        RestartButton.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
